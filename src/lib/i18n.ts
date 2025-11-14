ad95e4bb8832c3418ac122ead94ed4f0f939d482
Tolosa-appli <ensemblemidipyrenees@free.fr>
2025-11-14 10:49:36 +0100

    Update i18n.ts

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,8 +1,20 @@
 import fr from "@/locales/fr.json";
-import en from "@/locales/en.json";
-import de from "@/locales/de.json";
-import es from "@/locales/es.json";
-// ... importe toutes les autres langues
+import fr from "@/locales/de.json";
+import fr from "@/locales/en.json";
+import fr from "@/locales/ar.json";
+import fr from "@/locales/eu.json";
+import fr from "@/locales/zh-CN.json";
+import fr from "@/locales/es.json";
+import fr from "@/locales/fa.json";
+import fr from "@/locales/hi.json";
+import fr from "@/locales/it.json";
+import fr from "@/locales/ja.json";
+import fr from "@/locales/oc.json";
+import fr from "@/locales/pt.json";
+import fr from "@/locales/ru.json";
+import fr from "@/locales/tr.json";
+import fr from "@/locales/no.json";
+import fr from "@/locales/ro.json";
 
 const dictionaries = { fr, en, de, es /* ... */ };
 
