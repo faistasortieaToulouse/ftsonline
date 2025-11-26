3cb27130c7260f04ca36b6aeac2532c058047ed5
Tolosa-appli <ensemblemidipyrenees@free.fr>
2025-11-26 14:22:30 +0100

    Update route.ts

--- src/app/api/ut2-min/route.ts
+++ src/app/api/ut2-min/route.ts
@@ -14,7 +14,7 @@ url: string;
 source: string;
 }
 
-const BASE_URL = "[https://www.canal-u.tv/recherche?f%5B0%5D=chaine:375&sort_by=field_date_publication_1](https://www.canal-u.tv/recherche?f%5B0%5D=chaine:375&sort_by=field_date_publication_1)";
+const BASE_URL = "https://www.canal-u.tv/recherche?f%5B0%5D=chaine:375&sort_by=field_date_publication_1";
 
 async function fetchAllPages(): Promise<Event[]> {
 let page = 0;
