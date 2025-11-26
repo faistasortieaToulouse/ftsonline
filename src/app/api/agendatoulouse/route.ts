388eaff7d257b76182108cb3bea543d81fafa079
Tolosa-appli <ensemblemidipyrenees@free.fr>
2025-11-26 12:43:19 +0100

    Update route.ts

--- src/app/api/agendatoulouse/route.ts
+++ src/app/api/agendatoulouse/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 const API_ROUTES = [
-  "agenda-trad",
+  "agenda-trad-haute-garonne",
   "cultureenmouvements",
   "demosphere",
   "hautegaronne",
