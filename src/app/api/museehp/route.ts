88558d2aafa946bc3b9ff1e459f72ca03c9b9f90
Tolosa-appli <ensemblemidipyrenees@free.fr>
2025-12-16 16:26:12 +0100

    Create route.ts

---
+++ src/app/api/museehp/route.ts
@@ -0,0 +1,61 @@
+// src/app/api/museehp/route.ts
+import { NextResponse } from 'next/server';
+
+export interface MuseeHP {
+  commune: string;
+  nom: string;
+  categorie: string;
+  adresse: string;
+  url: string;
+  lat: number; 
+  lng: number; 
+}
+
+const museesHP: MuseeHP[] = [
+  // Coordonnées géographiques (lat/lng) sont des estimations
+  { commune: 'Argelès-Gazost', nom: 'Musée montagnard du Lavedan', adresse: '65400 Argelès-Gazost', categorie: 'Patrimoine/Histoire', url: 'https://www.argeles-gazost.com/musee-montagnard-du-lavedan-visiter-argeles-gazost/', lat: 43.0030, lng: -0.1010 },
+  { commune: 'Arras-en-Lavedan', nom: 'L\'Abbadiale d\'Arras-en-Lavedan', adresse: '1 Rue du Gabizos, 65400 Arras-en-Lavedan', categorie: 'Patrimoine/Religion', url: 'https://www.abbadiale.fr/', lat: 43.0080, lng: -0.0900 },
+  { commune: 'Bagnères-de-Bigorre', nom: 'Musée des Beaux-Arts Salies', adresse: 'Place des Thermes, 65200 Bagnères-de-Bigorre', categorie: 'Art', url: 'http://museesalies.fr/', lat: 43.0640, lng: 0.1450 },
+  { commune: 'Bagnères-de-Bigorre', nom: 'Muséum et Musée du marbre', adresse: 'Rue du Musée, 65200 Bagnères-de-Bigorre', categorie: 'Nature/Patrimoine', url: 'https://www.musees-midi-pyrenees.fr/musees/musee-du-marbre-et-du-museum/', lat: 43.0650, lng: 0.1460 },
+  { commune: 'Bagnères-de-Bigorre', nom: 'Musée Bigourdan du Vieux Moulin', adresse: 'Rue Hount-Blanque, 65200 Bagnères-de-Bigorre', categorie: 'Patrimoine/Histoire', url: 'http://www.museedefrance.fr/musee-bigourdan-du-vieux-moulin/', lat: 43.0660, lng: 0.1470 },
+  { commune: 'Bagnères-de-Bigorre', nom: 'Muséum d\'Histoire Naturelle de Bagnères de Bigorre', adresse: 'Rue du Musée, 65200 Bagnères-de-Bigorre', categorie: 'Nature', url: 'http://www.museum-bagneres.com/', lat: 43.0650, lng: 0.1460 },
+  { commune: 'Campan', nom: 'Carrière de marbre', adresse: '65710 Campan', categorie: 'Industrie/Patrimoine', url: 'https://www.campan.fr/musee/', lat: 43.0000, lng: 0.2000 },
+  { commune: 'Cauterets', nom: 'Musée 1900', adresse: 'Place Georges Clemenceau, 65110 Cauterets', categorie: 'Patrimoine/Histoire', url: 'https://www.musee1900-cauterets.com/', lat: 42.8880, lng: -0.1130 },
+  { commune: 'Gèdre', nom: 'Musée Millaris', adresse: '65120 Gèdre', categorie: 'Patrimoine/Histoire', url: 'https://www.tourisme-occitanie.com/musee-millaris/gedre/tabid/37151/offreid/742b71fa-12cd-416b-a279-88005b8109d9/detail.aspx', lat: 42.7560, lng: 0.0050 },
+  { commune: 'La Mongie', nom: 'Musée Salies des collections et beaux arts (Voir Bagnères)', adresse: '65200 La Mongie', categorie: 'Art/Patrimoine', url: 'http://museesalies.fr/', lat: 42.9240, lng: 0.2130 },
+  { commune: 'Beaudéan', nom: 'Epicerie d\'autrefois Chez Gabrielle', adresse: '349 Place de L\'église, 65710 Beaudéan', categorie: 'Patrimoine', url: 'https://www.tourisme-hautes-pyrenees.com/offre/fiche/epicerie-d-autrefois-chez-gabrielle/PCUMID065FS0002Q', lat: 43.0030, lng: 0.1760 },
+  { commune: 'Bagnères-de-Bigorre', nom: 'Museum d\'histoire naturelle (Voir Bagnères)', adresse: '65200 Bagnères-de-Bigorre', categorie: 'Nature', url: 'http://www.museum-bagneres.com/', lat: 43.0650, lng: 0.1460 },
+  { commune: 'Bagnères-de-Bigorre', nom: 'Musée de la vie quotidienne dans les Pyrénées', adresse: '7 boulevard Rolland Castells, 65200 Bagnères-de-Bigorre', categorie: 'Patrimoine/Histoire', url: 'https://www.tourisme-hautes-pyrenees.com/offre/fiche/musee-de-la-vie-quotidienne-dans-les-pyrenees/PCUMIP065V500SO2', lat: 43.0600, lng: 0.1400 },
+  { commune: 'Bagnères-de-Bigorre', nom: 'Musée Larrey', adresse: '65200 Bagnères-de-Bigorre', categorie: 'Histoire', url: 'http://musee-larrey.fr/', lat: 43.0590, lng: 0.1410 },
+  { commune: 'La Mongie', nom: 'Espace des expériences du Pic du Midi de Bigorre', adresse: '65200 La Mongie (sommet du Pic)', categorie: 'Science', url: 'https://www.picdumidi.com/', lat: 42.9360, lng: 0.1450 },
+  { commune: 'Bagnères-de-Bigorre', nom: 'Carrefour des patrimoines', adresse: '65200 Bagnères-de-Bigorre', categorie: 'Patrimoine', url: 'https://carrefourdespatrimoines.fr/', lat: 43.0605, lng: 0.1415 },
+  { commune: 'Aste', nom: 'Maison des Ferrère et baroque pyrénéen', adresse: '16, rue de l\'église, 65200 Aste', categorie: 'Patrimoine/Religion', url: 'https://www.tourisme-hautes-pyrenees.com/offre/fiche/maison-des-ferrere-et-du-baroque-pyreneen/PCUMID065FS0003B', lat: 43.0010, lng: 0.1340 },
+  { commune: 'Les Côteaux', nom: 'Musée du feu des sapeurs-pompiers', adresse: '65360 Les Côteaux', categorie: 'Industrie/Histoire', url: 'http://www.amicale-pompiers-ossun.com/musee.html', lat: 43.3440, lng: 0.0570 },
+  { commune: 'Monléon-Magnoac', nom: 'Notre Dame de Garaison', adresse: '65160 Monléon-Magnoac (Garaison)', categorie: 'Patrimoine/Religion', url: 'https://www.garaison.com/', lat: 43.3280, lng: 0.5400 },
+  { commune: 'Labatut-Rivière', nom: 'Mémoire des deux guerres en Sud-Ouest', adresse: '65700 Labatut-Rivière', categorie: 'Histoire', url: 'https://www.tourisme-occitanie.com/memoire-des-deux-guerres-en-sud-ouest/labatut-riviere/tabid/37151/offreid/72f44c21-f09c-4861-8406-03f7a4fa430b/detail.aspx', lat: 43.5350, lng: 0.0550 },
+  { commune: 'Maubourguet', nom: 'Musée archéologique du Val d\'Adour', adresse: '65700 Maubourguet', categorie: 'Patrimoine/Archéologie', url: 'https://www.tourisme-occitanie.com/musee-archeologique-du-val-dadour/maubourguet/tabid/37151/offreid/742b71fc-7b83-4a00-911e-7b71f92e2e8b/detail.aspx', lat: 43.5350, lng: 0.0200 },
+  { commune: 'Lourdes', nom: 'Musée Pyrénéen de Lourdes (Château Fort)', adresse: 'Château Fort de Lourdes, 65100 Lourdes', categorie: 'Patrimoine/Histoire', url: 'http://www.lourdes-chateaufort.com/', lat: 43.1000, lng: -0.0460 },
+  { commune: 'Lourdes', nom: 'Maison paternelle de Sainte-Bernadette Moulin Lacade', adresse: '16 Rue Bernadette Soubirous, 65100 Lourdes', categorie: 'Patrimoine/Religion', url: 'https://www.lourdes-infotourisme.com/moulin-lacade-maison-natale-de-sainte-bernadette/', lat: 43.0970, lng: -0.0520 },
+  { commune: 'Lourdes', nom: 'Musée du vélo', adresse: '65100 Lourdes', categorie: 'Sport/Patrimoine', url: 'https://www.museeduvello.com/', lat: 43.0980, lng: -0.0500 },
+  { commune: 'Lourdes', nom: 'Le Petit Lourdes', adresse: '65100 Lourdes', categorie: 'Patrimoine/Loisirs', url: 'https://www.lepetitlourdes.com/', lat: 43.1020, lng: -0.0480 },
+  { commune: 'Adé', nom: 'La féerie des eaux', adresse: '70 Rue de Lassalle, 65100 Adé', categorie: 'Loisirs', url: 'https://www.guide-toulouse-pyrenees.com/fr/tourisme/decouvrir/sites-touristiques/jardins-parcs/ade-923/la-feerie-des-eaux-1499.html', lat: 43.1500, lng: -0.0500 },
+  { commune: 'Lourdes', nom: 'Musée Christhi', adresse: '24, Rue de la Grotte, 65100 Lourdes', categorie: 'Histoire/Religion', url: 'https://www.tourism-lourdes.com/fr/sites-touristiques-lourdes/musees/le-musee-christi.html', lat: 43.0995, lng: -0.0470 },
+  { commune: 'Lourdes', nom: 'Le Cachot', adresse: 'Rue des Petits Fossés, 65100 Lourdes', categorie: 'Religion', url: 'https://www.lourdes-infotourisme.com/le-cachot-maison-natale-de-sainte-bernadette/', lat: 43.0985, lng: -0.0485 },
+  { commune: 'Lourdes', nom: 'Hospice Sainte-Bernadette', adresse: '2 Av. Alexandre Marqui, 65100 Lourdes', categorie: 'Religion', url: 'https://www.lourdes-infotourisme.com/offres/hospice-sainte-bernadette-lourdes-fr-3152360/', lat: 43.0990, lng: -0.0480 },
+  { commune: 'Lourdes', nom: 'Musée de cire', adresse: '8 Avenue du Paradis, 65100 Lourdes', categorie: 'Art/Loisirs', url: 'http://www.musee-cire-lourdes.com/', lat: 43.0980, lng: -0.0475 },
+  { commune: 'Lourdes', nom: 'Musée Sainte-Bernadette', adresse: '93 Bd Rémi Sempé, 65100 Lourdes', categorie: 'Religion', url: 'https://www.lourdes-infotourisme.com/offres/musee-sainte-bernadette-lourdes-fr-3152355/', lat: 43.0992, lng: -0.0478 },
+  { commune: 'Lourdes', nom: 'Château-fort et musée pyrénéen de Lourdes (Ancien nom: Château de Soum)', adresse: '65100 Lourdes', categorie: 'Patrimoine/Histoire', url: 'http://www.lourdes-chateaufort.com/', lat: 43.1000, lng: -0.0460 },
+  { commune: 'Lourdes', nom: 'Basilique de l\'Immaculée-Conception', adresse: '65100 Lourdes (Sanctuaire)', categorie: 'Religion', url: 'https://www.lourdes-france.org/', lat: 43.0980, lng: -0.0550 },
+  { commune: 'Lourdes', nom: 'Basilique souterraine Saint-Pie X', adresse: '65100 Lourdes (Sanctuaire)', categorie: 'Religion', url: 'https://www.lourdes-france.org/', lat: 43.0975, lng: -0.0555 },
+  { commune: 'Lourdes', nom: 'Crypte du sanctuaire de Lourdes', adresse: '65100 Lourdes (Sanctuaire)', categorie: 'Religion', url: 'https://www.lourdes-france.org/', lat: 43.0970, lng: -0.0550 },
+  { commune: 'Lourdes', nom: 'Eglise du Sacré-Coeur de Lourdes', adresse: '65100 Lourdes', categorie: 'Religion', url: '', lat: 43.0965, lng: -0.0490 },
+  { commune: 'Lourdes', nom: 'Basilique Notre-Dame du Rosaire de Lourdes', adresse: '65100 Lourdes (Sanctuaire)', categorie: 'Religion', url: 'https://www.lourdes-france.org/', lat: 43.0960, lng: -0.0560 },
+  { commune: 'Lourdes', nom: 'Église Sainte-Bernadette de Lourdes', adresse: '65100 Lourdes (Sanctuaire)', categorie: 'Religion', url: 'https://www.lourdes-france.org/', lat: 43.0955, lng: -0.0565 },
+  { commune: 'Lourdes', nom: 'Musée de la Nativité de Lourdes', adresse: '21 Quai Saint-Jean, 65100 Lourdes', categorie: 'Art/Religion', url: 'https://www.tourism-lourdes.com/fr/sites-touristiques-lourdes/musees/le-musee-de-la-nativite.html', lat: 43.0950, lng: -0.0450 },
+];
+
+export async function GET() {
+  return NextResponse.json(museesHP);
+}
+
+export type Musee = MuseeHP;
