8ef44a4fd24006522b614f227bee50a78f7ffa5b
Tolosa-appli <ensemblemidipyrenees@free.fr>
2026-01-26 15:22:37 +0100

    Update route.ts

--- src/app/api/train1euro/route.ts
+++ src/app/api/train1euro/route.ts
@@ -11,28 +11,35 @@ export async function GET() {
     },
     {
       id: 2,
-      title: "Fairtiq",
+      title: "Fairtiq, trouve un billet à 1€",
       url: "https://fairtiq.com/fr/",
       description: "L'application pour voyager sans billet papier, idéale pour les trajets lio.",
       category: "Application"
     },
     {
       id: 3,
-      title: "SNCF Connect",
+      title: "SNCF Connect, découvre chaque jour des billets à 1€",
       url: "https://www.sncf-connect.com/",
       description: "Achat de billets et consultation des horaires en temps réel.",
       category: "Réservation"
     },
     {
       id: 4,
-      title: "Trainline",
+      title: "Trainline, trouve des train à 1€",
       url: "https://www.thetrainline.com/fr/compagnies-ferroviaires/ter/train-1-euro",
       description: "Alternative pour comparer et acheter vos billets TER à petit prix.",
       category: "Comparateur"
     },
     {
       id: 5,
-      title: "Tourisme Occitanie",
+      title: "Tourisme Occitanie, premier week-end, billets de train à 1€",
+      url: "https://www.tourisme-occitanie.com/pratique/transports/venir-en-train/profitez-des-week-ends-lio-train-a-1-euro/",
+      description: "Informations pratiques sur les week-ends à 1 euro et idées de sorties.",
+      category: "Infos & Tourisme"
+    },
+    {
+      id: 5,
+      title: "Tourisme Occitanie, LIO, billet de train à 1€",
       url: "https://www.tourisme-occitanie.com/pratique/transports/venir-en-train/profitez-des-week-ends-lio-train-a-1-euro/",
       description: "Informations pratiques sur les week-ends à 1 euro et idées de sorties.",
       category: "Infos & Tourisme"
