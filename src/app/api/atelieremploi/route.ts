4c1f799b17ca86e15ebf77963492a89db8ca81aa
Tolosa-appli <ensemblemidipyrenees@free.fr>
2026-01-26 15:53:50 +0100

    Update route.ts

--- src/app/api/atelieremploi/route.ts
+++ src/app/api/atelieremploi/route.ts
@@ -4,7 +4,7 @@ export async function GET() {
   const resources = [
     {
       id: 1,
-      title: "Dossier Complet : Atelier Emploi",
+      title: "Dossier Complet : Atelier Emploi sur le Drive",
       url: "https://drive.google.com/drive/folders/1cqy4Kmu9ZLDtiWRoQZvm69qrHsnBoKs1?usp=drive_link",
       description: "Accédez à l'ensemble des documents, guides et supports pour booster votre recherche d'emploi.",
       category: "Ressources Google Drive",
