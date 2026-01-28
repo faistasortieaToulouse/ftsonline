a35df79b4829c083ce5fbe4105f51c77c64f1ca4
Tolosa-appli <ensemblemidipyrenees@free.fr>
2026-01-28 09:47:33 +0100

    Update route.ts

--- src/app/api/constellation/route.ts
+++ src/app/api/constellation/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import stars from '@/data/constellation/stars.json';
+import stars from '../../../data/constellation/stars.json';
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
