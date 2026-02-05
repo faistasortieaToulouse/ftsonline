9951cec97441085c5632a70749714c9077470ad0
Tolosa-appli <ensemblemidipyrenees@free.fr>
2026-02-05 14:26:12 +0100

    Update route.ts

--- src/app/api/afriquedusud/route.ts
+++ src/app/api/afriquedusud/route.ts
@@ -2,23 +2,15 @@ import { NextResponse } from 'next/server';
 import path from 'path';
 import { promises as fs } from 'fs';
 
-// Interface pour typer les données
-export interface VilleAfriqueSud {
-  ville: string;
-  role: string;
-  province: string;
-  population_agglo: number;
-}
-
 export async function GET() {
   try {
     const jsonDirectory = path.join(process.cwd(), 'data/monde');
-    const fileContents = await fs.readFile(jsonDirectory + '/afriquedusud.json', 'utf8');
-    const data: VilleAfriqueSud[] = JSON.parse(fileContents);
+    const filePath = path.join(jsonDirectory, 'afriquedusud.json');
+    const fileContents = await fs.readFile(filePath, 'utf8');
+    const data = JSON.parse(fileContents);
 
     return NextResponse.json(data);
   } catch (error) {
-    console.error("Erreur API Afrique du Sud:", error);
-    return NextResponse.json({ error: "Erreur de chargement" }, { status: 500 });
+    return NextResponse.json({ error: "Erreur de chargement JSON" }, { status: 500 });
   }
 }
