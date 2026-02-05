d2c478d87e46bea32358370091786fb9f4600251
Tolosa-appli <ensemblemidipyrenees@free.fr>
2026-02-05 12:50:49 +0100

    Update route.ts

--- src/app/api/chine/route.ts
+++ src/app/api/chine/route.ts
@@ -5,11 +5,13 @@ import { promises as fs } from 'fs';
 export async function GET() {
   try {
     const jsonDirectory = path.join(process.cwd(), 'data/monde');
-    const fileContents = await fs.readFile(jsonDirectory + '/chine.json', 'utf8');
+    const filePath = path.join(jsonDirectory, 'chine.json');
+    const fileContents = await fs.readFile(filePath, 'utf8');
     const data = JSON.parse(fileContents);
 
     return NextResponse.json(data);
   } catch (error) {
-    return NextResponse.json({ error: "Erreur de lecture du JSON" }, { status: 500 });
+    console.error("API Chine Error:", error);
+    return NextResponse.json({ error: "Erreur de lecture des données" }, { status: 500 });
   }
 }
