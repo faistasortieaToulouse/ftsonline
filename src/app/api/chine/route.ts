0ec148f20c4f69a9d9f09f975050d40993d0d41a
Tolosa-appli <ensemblemidipyrenees@free.fr>
2026-02-05 11:30:18 +0100

    Update route.ts

--- src/app/api/chine/route.ts
+++ src/app/api/chine/route.ts
@@ -2,38 +2,14 @@ import { NextResponse } from 'next/server';
 import path from 'path';
 import { promises as fs } from 'fs';
 
-export interface TerritoireChine {
-  nom: string;
-  capitale?: string;
-  population_est_2026?: string;
-  population_totale_2026?: string;
-}
-
-export interface VilleUrbaineChine {
-  ville: string;
-  province: string;
-  census_2020: number;
-}
-
-export interface DonneesChine {
-  pays: string;
-  administration_territoriale: {
-    provinces: TerritoireChine[];
-    regions_autonomes: TerritoireChine[];
-    municipalites: TerritoireChine[];
-  };
-  villes_principales_urbaines: VilleUrbaineChine[];
-}
-
 export async function GET() {
   try {
     const jsonDirectory = path.join(process.cwd(), 'data/monde');
     const fileContents = await fs.readFile(jsonDirectory + '/chine.json', 'utf8');
-    const data: DonneesChine = JSON.parse(fileContents);
+    const data = JSON.parse(fileContents);
 
     return NextResponse.json(data);
   } catch (error) {
-    console.error("Erreur API Chine:", error);
-    return NextResponse.json({ error: "Erreur de chargement" }, { status: 500 });
+    return NextResponse.json({ error: "Erreur de lecture du JSON" }, { status: 500 });
   }
 }
