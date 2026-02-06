3f45e72f6aff948c5bf18ec8649c372b800a1e95
Tolosa-appli <ensemblemidipyrenees@free.fr>
2026-02-06 09:13:19 +0100

    Create route.ts

---
+++ src/app/api/inde/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from 'next/server';
+import path from 'path';
+import { promises as fs } from 'fs';
+
+export async function GET() {
+  try {
+    const jsonDirectory = path.join(process.cwd(), 'data/monde');
+    const fileContents = await fs.readFile(jsonDirectory + '/inde.json', 'utf8');
+    const data = JSON.parse(fileContents);
+
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json({ error: "Erreur de chargement" }, { status: 500 });
+  }
+}
