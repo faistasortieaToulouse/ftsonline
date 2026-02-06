b5933fcfc6654f004979adeef7ccb4d22e3d0803
Tolosa-appli <ensemblemidipyrenees@free.fr>
2026-02-06 10:45:58 +0100

    Create route.ts

---
+++ src/app/api/perou/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from 'next/server';
+import path from 'path';
+import { promises as fs } from 'fs';
+
+export async function GET() {
+  try {
+    const jsonPath = path.join(process.cwd(), 'data', 'monde', 'perou.json');
+    const fileContents = await fs.readFile(jsonPath, 'utf8');
+    const data = JSON.parse(fileContents);
+
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json({ error: "Fichier non trouvé" }, { status: 404 });
+  }
+}
