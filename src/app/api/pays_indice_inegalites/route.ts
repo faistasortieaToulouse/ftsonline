66d77e75ef0aabe8fd643021f555f37370814f69
Tolosa-appli <ensemblemidipyrenees@free.fr>
2026-02-13 15:44:05 +0100

    Update route.ts

--- src/app/api/pays_indice_inegalites/route.ts
+++ src/app/api/pays_indice_inegalites/route.ts
@@ -1,15 +1,15 @@
-import { NextResponse } from 'next/server';
-import fs from 'fs';
-import path from 'path';
+import { NextResponse } from "next/server";
+import fs from "fs";
+import path from "path";
 
 export async function GET() {
   try {
     const filePath = path.join(
       process.cwd(),
-      'data/statistiques/tri_pays_indice_inegalites.json'
+      "data/statistiques/tri_pays_indice_inegalites.json"
     );
 
-    const fileContents = fs.readFileSync(filePath, 'utf8');
+    const fileContents = fs.readFileSync(filePath, "utf8");
     const data = JSON.parse(fileContents);
 
     return NextResponse.json(data);
@@ -18,7 +18,7 @@ export async function GET() {
     console.error("Erreur API Inégalités :", error);
 
     return NextResponse.json(
-      { error: "Erreur lors du chargement des données" },
+      { error: "Impossible de charger les données", data: [] },
       { status: 500 }
     );
   }
