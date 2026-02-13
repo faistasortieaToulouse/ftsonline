7a1e46f754ab81721dee7397b45f31d14a03060e
Tolosa-appli <ensemblemidipyrenees@free.fr>
2026-02-13 10:09:47 +0100

    Create route.ts

---
+++ src/app/api/pays_mecanique_agricole/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from 'next/server';
+import fs from 'fs';
+import path from 'path';
+
+export async function GET() {
+  try {
+    const filePath = path.join(process.cwd(), 'data/statistiques/tri_pays_mecanique_agricole.json');
+    const fileContents = fs.readFileSync(filePath, 'utf8');
+    const data = JSON.parse(fileContents);
+    
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json({ error: "Erreur de chargement" }, { status: 500 });
+  }
+}
