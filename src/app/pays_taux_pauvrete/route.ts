7afe8511426b4e09ef8a0e51fbedeb7748657844
Tolosa-appli <ensemblemidipyrenees@free.fr>
2026-02-13 13:02:52 +0100

    Delete src/app/pays_taux_pauvrete/route.ts

--- src/app/pays_taux_pauvrete/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from 'next/server';
-import povertyData from '../../../../data/statistiques/tri_pays_taux_pauvrete.json';
-
-export async function GET() {
-  return NextResponse.json(povertyData);
-}
