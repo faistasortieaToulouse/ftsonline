b6205d44efc6da957a9383f16f16a0fd185bd3fd
Tolosa-appli <ensemblemidipyrenees@free.fr>
2026-02-13 10:19:52 +0100

    Create route.ts

---
+++ src/app/api/pays_natalite_fecondite/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from 'next/server';
+import fs from 'fs';
+import path from 'path';
+
+export async function GET() {
+  try {
+    const filePath = path.join(process.cwd(), 'data/statistiques/tri_pays_natalite_fecondite.json');
+    const fileContents = fs.readFileSync(filePath, 'utf8');
+    const data = JSON.parse(fileContents);
+    
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json({ error: "Erreur de chargement des données démographiques" }, { status: 500 });
+  }
+}
