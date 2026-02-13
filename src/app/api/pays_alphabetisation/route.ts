3405a2d17bdbf06c60eec2f0b78bb50dc4fd81be
Tolosa-appli <ensemblemidipyrenees@free.fr>
2026-02-13 09:45:08 +0100

    Create route.ts

---
+++ src/app/api/pays_alphabetisation/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from 'next/server';
+import fs from 'fs';
+import path from 'path';
+
+export async function GET() {
+  try {
+    const filePath = path.join(process.cwd(), 'data/statistiques/tri_pays_alphabetisation.json');
+    const fileContents = fs.readFileSync(filePath, 'utf8');
+    const data = JSON.parse(fileContents);
+    
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json({ error: "Erreur lors du chargement des données" }, { status: 500 });
+  }
+}
