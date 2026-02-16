914fd4880a6de66e3fd9d39dc49f6fe4b7f1741b
Tolosa-appli <ensemblemidipyrenees@free.fr>
2026-02-16 10:04:29 +0100

    Create route.ts

---
+++ src/app/api/paystest/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from 'next/server';
+// Importation directe du fichier JSON
+import inequalityData from '@/data/statistiques/tri_pays_indice_inegalites.json';
+
+export async function GET() {
+  try {
+    // On retourne directement les données importées
+    return NextResponse.json(inequalityData);
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Impossible de charger les données statistiques" },
+      { status: 500 }
+    );
+  }
+}
