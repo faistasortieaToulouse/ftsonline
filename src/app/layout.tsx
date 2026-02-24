7037ec49e86697e417489b7e8afd600cf15fadcc
Tolosa-appli <ensemblemidipyrenees@free.fr>
2026-02-24 15:22:26 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,9 +44,30 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+const gaId = process.env.NEXT_PUBLIC_GA_ID;
   return (
     <html lang="fr" suppressHydrationWarning>
       <head>
+        {/* --- GOOGLE ANALYTICS VIA VARIABLES D'ENVIRONNEMENT --- */}
+        {gaId && (
+          <>
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
+              strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', '${gaId}');
+              `}
+            </Script>
+          </>
+        )}
+        {/* ------------------------------------------------------ */}
+
         {/* Préconnexion aux polices pour la performance */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
