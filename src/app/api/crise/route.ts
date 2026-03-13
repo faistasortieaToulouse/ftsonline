ddfbda8114ff3ec3c950d9e3716f601d9be9b6b5
Tolosa-appli <ensemblemidipyrenees@free.fr>
2026-03-13 14:22:56 +0100

    Create route.ts

---
+++ src/app/api/crise/route.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  try {
+    return NextResponse.json({
+      titre: "Crises Monétaires et Financières",
+      source: "https://fr.wikipedia.org/wiki/Liste_des_crises_mon%C3%A9taires_et_financi%C3%A8res"
+    });
+  } catch (error) {
+    return NextResponse.json({ error: "Erreur lors de la récupération du lien" }, { status: 500 });
+  }
+}
