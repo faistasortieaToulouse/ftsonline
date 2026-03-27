fea266ba04496e6482cf80f9e2998da268dd9cc9
Tolosa-appli <ensemblemidipyrenees@free.fr>
2026-03-27 13:11:11 +0100

    Update route.ts

--- src/app/api/langue/route.ts
+++ src/app/api/langue/route.ts
@@ -25,13 +25,27 @@ export async function GET() {
     },
     {
       id: 4,
+      title: "Café des langues au Terminus des Prétentieux",
+      url: "https://www.ville-colomiers.fr/1/mon-quotidien/espace-citoyens/terminus-des-pretentieux",
+      description: "Café associatif à Colomiers qui organise un café des langues.",
+      tag: "Portail"
+    },
+        {
+      id: 5,
+      title: "Café des langues à Colomiers",
+      url: "https://www.facebook.com/auterminusdespretentieux/",
+      description: "Café des langues le mercredi de 14h à 16h.",
+      tag: "Portail"
+    },
+    {
+      id: 6,
       title: "Déclic Toulousain",
       url: "http://www.happypeople.biz.st/",
       description: "Happy People, Site Web ou sur Facebook.",
       tag: "Portail"
     },
     {
-      id: 5,
+      id: 7,
       title: "Atelier FLE sur le Drive",
       url: "https://drive.google.com/drive/folders/1_V-3M7AXzYW1ix1lbS4n_wrnRjrZceo8?usp=drive_link",
       description: "Français Langue Étrangère : supports et ressources pédagogiques.",
