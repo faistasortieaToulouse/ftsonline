a2abad133f9223d504654ec972503a0899f23230
Tolosa-appli <ensemblemidipyrenees@free.fr>
2026-04-29 16:07:44 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -85,7 +85,7 @@ const nbHierarchie = 23;
 const nbSaHistoire = 13;
 const nbLangue = 2;
 const nbMondeEconomie = 5;
-const nbMondeGeo = 4;
+const nbMondeGeo = 5;
 const nbMondeSocio = 6;
 const nbMondeHistoire = 7;
 const nbMondeNiveauVie = 16;
@@ -618,10 +618,11 @@ const categories = [
     { title: "Mapmonde", href: "/mapmonde" },
 	{ title: "Fuseaux horaires", href: "/fuseaux" },
 	{ title: "Géographie du Monde", href: "/geomonde" },
+	{ title: "Micro-Etats et Nations", href: "/microetatnation" },
     { title: "Empires coloniaux", href: "/colonies" }
   ]
 },
-// const nbMondeGeo = 4;
+// const nbMondeGeo = 5;
 	
 {  
   title: "Savoirs : Monde, histoire", 
